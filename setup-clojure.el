567c2ac4041c6692dbaaedc82ed4dc5c66b5fe61
Glen Stampoultzis <gstamp@gmail.com>
2012-07-04 16:56:02 +1000

    Fixed transpose words in clojure so that it treats hyphenated words as whole-words.

--- setup-clojure.el
+++ setup-clojure.el
@@ -14,6 +14,22 @@
      (it 'defun)
      (do-it 'defun)))
 
+
+;;Treat hyphens as a word character when transposing words
+(defvar clojure-mode-with-hyphens-as-word-sep-syntax-table
+  (let ((st (make-syntax-table clojure-mode-syntax-table)))
+    (modify-syntax-entry ?- "w" st)
+    st))
+
+(defun live-transpose-words-with-hyphens (arg)
+  "Treat hyphens as a word character when transposing words"
+  (interactive "*p")
+  (with-syntax-table clojure-mode-with-hyphens-as-word-sep-syntax-table
+    (transpose-words arg)))
+
+(define-key clojure-mode-map (kbd "M-t") 'live-transpose-words-with-hyphens)
+
+
 ;; Redirect output from other threads.
 ;; Disabled - enabling this seems to cause bugs in slime
 ;; (add-hook 'slime-mode-hook 'slime-redirect-inferior-output)
