f24431ebaa83e70de42eb77b313f298f4ba0528e
Glen Stampoultzis <gstamp@gmail.com>
2012-07-16 15:02:20 +1000

    Handle image capture better in org mode

--- setup-orgmode.el
+++ setup-orgmode.el
@@ -4,6 +4,8 @@
   "Take a screenshot into a time stamped unique-named file in the
 same directory as the org-buffer and insert a link to this file."
   (interactive)
+  (if (not (file-exists-p "images"))
+      (make-directory "images"))
   (setq filename
         (concat
          (make-temp-name
@@ -12,7 +14,9 @@ same directory as the org-buffer and insert a link to this file."
                   (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
   (setq just-filename (car (last (split-string filename "/") )))
   (call-process "import" nil nil nil filename)
-  (insert (concat "[[./" just-filename "]]"))
+  (rename-file filename "images/")
+  (insert (concat "[[./images/" just-filename "]]"))
   (org-display-inline-images))
 
 (provide 'setup-orgmode)
+
