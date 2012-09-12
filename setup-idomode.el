e06e7faccf6b6d4e0bd4f9e597a1cbb2da34ae33
Glen Stampoultzis <gstamp@gmail.com>
2012-09-12 15:12:21 +1000

    Ido nil pointer guard

--- setup-idomode.el
+++ setup-idomode.el
@@ -9,11 +9,12 @@
 (add-hook 'ido-make-dir-list-hook 'ido-sort-mtime)
 (defun ido-sort-mtime ()
   (setq ido-temp-list
-        (sort ido-temp-list 
-              (lambda (a b)
-                (time-less-p
-                 (sixth (file-attributes (concat ido-current-directory b)))
-                 (sixth (file-attributes (concat ido-current-directory a)))))))
+        (if ido-temp-list
+            (sort ido-temp-list 
+                  (lambda (a b)
+                    (time-less-p
+                     (sixth (file-attributes (concat ido-current-directory b)))
+                     (sixth (file-attributes (concat ido-current-directory a))))))))
   (ido-to-end ;; move . files to end (again)
    (delq nil (mapcar
               (lambda (x) (and (char-equal (string-to-char x) ?.) x))
