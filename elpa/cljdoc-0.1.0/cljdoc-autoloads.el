e3d6bed7ae50fab37997c42722a7119c1d72bb4c
Glen Stampoultzis <Glen.Stampoultzis@sensis.com.au>
2012-11-12 12:21:41 +1100

    Checking all the ELPA packages.
    This bloats the repository but means that I can have confidence that
    when I clone a new instance that I will have a working emacs instance.
    Unfortunately too many of the packages in Marmalade were broken so I
    can't count on it.

---
+++ elpa/cljdoc-0.1.0/cljdoc-autoloads.el
@@ -0,0 +1,18 @@
+;;; cljdoc-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("cljdoc-pkg.el" "cljdoc.el") (20441 8669
+;;;;;;  9282))
+
+;;;***
+
+(provide 'cljdoc-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; cljdoc-autoloads.el ends here
---
+++ elpa/popup-0.5/popup-autoloads.el
@@ -0,0 +1,18 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (20461 9873
+;;;;;;  282640))
+
+;;;***
+
+(provide 'popup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; popup-autoloads.el ends here
