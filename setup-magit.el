198c11fbeebb4e61cddf5c6f90e34d558b73358f
Glen Stampoultzis <Glen.Stampoultzis@sensis.com.au>
2013-01-05 16:57:35 +1100

    Toggle whitespace in magit

--- setup-magit.el
+++ setup-magit.el
@@ -1,4 +1,3 @@
-(provide 'setup-magit)
 
 ;; full screen magit-status
 
@@ -17,3 +16,27 @@
   (jump-to-register :magit-fullscreen))
 
 (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+
+
+;; This adds W to toggle ignoring whitespace in magit.
+
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
+
+
+(provide 'setup-magit)
