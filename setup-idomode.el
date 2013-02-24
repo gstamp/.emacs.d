6eaec71e12525948e3a40c34a717638aa342bad1
Glen Stampoultzis <Glen.Stampoultzis@sensis.com.au>
2013-02-25 10:28:31 +1100

    Open file list vertically
    We tried this previously and needed to roll it back.  Hopefully this
    works better this time. Also made open in project use full path since
    we now have the space to display it.

--- setup-idomode.el
+++ setup-idomode.el
@@ -1,5 +1,14 @@
 
-;; Deleted old configuration because it was causing me too many
-;; problems. Keep this around in case I need to add future modifications.
+;; Make ido open file list vertically
+(setq ido-max-prospects 60)
+(setq ido-max-file-prompt-width 0.8)
+(setq ido-max-window-height 30)
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+(defun ido-disable-line-truncation () (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-truncation)
+
+(setq ffip-full-paths 't) ; Not really part of ido but works well with
+                          ; vertically displayed lists.
+
 
 (provide 'setup-idomode)
