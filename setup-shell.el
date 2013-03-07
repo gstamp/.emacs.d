21453e29adae2ea2e722266b7d24a1579650695a
Glen Stampoultzis <Glen.Stampoultzis@sensis.com.au>
2013-03-07 11:31:19 +1100

    Make eshell show current git branch

--- setup-shell.el
+++ setup-shell.el
@@ -12,4 +12,52 @@
             (define-key shell-mode-map
               (kbd "C-d") 'comint-delchar-or-eof-or-kill-buffer)))
 
+(defun pwd-repl-home (pwd)
+  (interactive)
+  (let* ((home (expand-file-name (getenv "HOME")))
+         (home-len (length home)))
+    (if (and
+         (>= (length pwd) home-len)
+         (equal home (substring pwd 0 home-len)))
+        (concat "~" (substring pwd home-len))
+      pwd)))
+
+(defun curr-dir-git-branch-string (pwd)
+  "Returns current git branch as a string, or the empty string if
+PWD is not in a git repo (or the git command is not found)."
+  (interactive)
+  (when (and (eshell-search-path "git")
+             (locate-dominating-file pwd ".git"))
+    (let ((git-output (shell-command-to-string (concat "cd " pwd " && git branch | grep '\\*' | sed -e 's/^\\* //'"))))
+      (propertize (concat "["
+                          (if (> (length git-output) 0)
+                              (substring git-output 0 -1)
+                            "(no branch)")
+                          "]") 'face `(:foreground "green"))
+      )))
+
+(setq eshell-prompt-function
+      (lambda ()
+        (concat
+         (propertize ((lambda (p-lst)
+                        (if (> (length p-lst) 3)
+                            (concat
+                             (mapconcat (lambda (elm) (if (zerop (length elm)) ""
+                                                        (substring elm 0 1)))
+                                        (butlast p-lst 3)
+                                        "/")
+                             "/"
+                             (mapconcat (lambda (elm) elm)
+                                        (last p-lst 3)
+                                        "/"))
+                          (mapconcat (lambda (elm) elm)
+                                     p-lst
+                                     "/")))
+                      (split-string (pwd-repl-home (eshell/pwd)) "/")) 'face `(:foreground "yellow"))
+         (or (curr-dir-git-branch-string (eshell/pwd)))
+         (propertize "# " 'face 'default))))
+
+(setq eshell-highlight-prompt nil)
+
+
 (provide 'setup-shell)
