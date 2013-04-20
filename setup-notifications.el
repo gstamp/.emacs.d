4e3ab53ae252e4a0677a13670abddb83edde8e9e
Glen Stampoultzis <Glen.Stampoultzis@sensis.com.au>
2013-04-20 21:48:39 +1000

    Somehow some of the setup got pasted into setup-notifications.el by accident.

--- setup-notifications.el
+++ setup-notifications.el
@@ -13,33 +13,4 @@
   (when (eq match-type 'current-nick)
     (osx-notify (buffer-name (current-buffer)) message)))
 
-
-(when (equal system-type 'darwin)
-  (progn
-    ;; map meta to the command key on mac
-    (setq mac-option-key-is-meta nil)
-    (setq mac-left-command-key-is-meta t)
-    (setq mac-command-modifier 'meta)
-    (setq mac-option-modifier nil)
-
-    ;; I like the right option key to be control because there's no
-    ;; right control key on the mac
-    (setq mac-right-option-modifier 'control)
-
-    ;; sets fn-delete to be right-delete
-    (global-set-key [kp-delete] 'delete-char)
-
-    ;; keybinding to toggle full screen mode
-    (global-set-key (quote [M-f10]) (quote ns-toggle-fullscreen))
-
-    ;; Move to trash when deleting stuff
-    (setq delete-by-moving-to-trash t
-          trash-directory "~/.Trash/emacs")
-
-    ;; mac friendly font
-    (if window-system
-        (set-face-attribute 'default nil :font "Monaco-16"))
-
-    (add-hook 'erc-text-matched-hook 'osx-notify-erc-hook)))
-
 (provide 'setup-notifications)
