306182d792954c8059972b1c977996930c3e1389
Glen Stampoultzis <gstamp@gmail.com>
2013-04-24 14:55:51 +1000

    Minor refactor - nothing to see here.

--- setup-git-gutter.el
+++ setup-git-gutter.el
@@ -1,9 +1,10 @@
 (require 'git-gutter)
 
+(defun git-gutter-save-hook ()
+  (when (zerop (call-process-shell-command "git rev-parse --show-toplevel"))
+    (git-gutter)))
+
 ;; Update changes information after save buffer
-(add-hook 'after-save-hook
-          (lambda ()
-            (when (zerop (call-process-shell-command "git rev-parse --show-toplevel"))
-              (git-gutter))))
+(add-hook 'after-save-hook 'git-gutter-save-hook)
 
 (provide 'setup-git-gutter)
