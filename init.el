2ac017152853a3ef75178e26474b3e3f8ca87e01
Glen Stampoultzis <gstamp@gmail.com>
2013-05-31 10:13:21 +1000

    Remove recreating scratch buffer since it wasn't working anyway

--- init.el
+++ init.el
@@ -429,27 +429,6 @@ in the sexp, not the end of the current one."
        
        )
 
-;; If the *scratch* buffer is killed, recreate it automatically
-(defun kill-scratch-buffer ()
-  ;; The next line is just in case someone calls this manually
-  (set-buffer (get-buffer-create "*scratch*"))
-  ;; Kill the current (*scratch*) buffer
-  (remove-hook 'kill-buffer-query-functions 'kill-scratch-buffer)
-  (kill-buffer (current-buffer))
-  ;; Make a brand new *scratch* buffer
-  (set-buffer (get-buffer-create "*scratch*"))
-  (lisp-interaction-mode)
-  (make-local-variable 'kill-buffer-query-functions)
-  (add-hook 'kill-buffer-query-functions 'kill-scratch-buffer)
-  ;; Since we killed it, don't let caller do that.
-  nil)
-(after 'emacs-lisp 
-       (save-excursion
-         (set-buffer (get-buffer-create "*scratch*"))
-         (lisp-interaction-mode)
-         (make-local-variable 'kill-buffer-query-functions)
-         (add-hook 'kill-buffer-query-functions 'kill-scratch-buffer)))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Setup: Git Gutter
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
